b72f645079e4a46ecee989af3e264e506fdbccdb
Roman Bienov <rodergstep@gmail.com>
2016-10-17 12:17:03 +0300

    optimization

--- js/script.js
+++ js/script.js
@@ -1,45 +1,48 @@
 function getData(url, type) {
-  var httpreq = new XMLHttpRequest();
-  httpreq.open("GET", url, false);
-  httpreq.send(null);
-  if (type == 'text') {
-    return httpreq.responseText.split(', ');
-  }
-  if (type == 'json') {
-    return JSON.parse(httpreq.responseText);
-  }
+    var httpreq = new XMLHttpRequest();
+    httpreq.open("GET", url, false);
+    httpreq.send(null);
+    if (type == 'text') {
+        return httpreq.responseText.split(', ');
+    }
+    if (type == 'json') {
+        return JSON.parse(httpreq.responseText);
+    }
 }
 
 var data = getData('js/data.json', 'json');
 
-var unSkills = [];
-var namesByFriends = [];
+var uniqueSkills = [];
+var quantityofFriends = [];
 var friendsCount = [];
-var unFriends = [];
+var allFriends = [];
 
 console.log("People:");
-_.forEach(data, function (value) {
-  console.log(value);
-  unSkills = _.union(unSkills, value.skills);
-  friendsCount.push({name: value.name, friends: value.friends.length});
+_.forEach(data, function(value) {
+    console.log(value);
+    uniqueSkills = _.union(uniqueSkills, value.skills);
+    friendsCount.push({
+        name: value.name,
+        friends: value.friends.length
+    });
 
-  _.forEach(value.friends, function (value1) {
-    unFriends = _.concat(unFriends, value1.name);
-  });
+    _.forEach(value.friends, function(value1) {
+        allFriends = _.concat(allFriends, value1.name);
+    });
 });
 
 friendsCount = _.sortBy(friendsCount, 'friends');
 
-_.forEach(friendsCount, function (value) {
-  namesByFriends.push(value.name)
+_.forEach(friendsCount, function(value) {
+    quantityofFriends.push(value.name);
 });
 
 console.log('');
 console.log("Unique skills:");
-console.log(_.sortBy(unSkills));
+console.log(_.sortBy(uniqueSkills));
 console.log('');
-console.log("Names by friends:");
-console.log(namesByFriends);
+console.log("Quantity of friends:");
+console.log(quantityofFriends);
 console.log('');
-console.log("Unique friends:");
-console.log(_.uniq(unFriends));
+console.log("All friends:");
+console.log(_.uniq(allFriends));
